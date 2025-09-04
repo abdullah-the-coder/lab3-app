306c14ea3427aa2971db31b523e45348c2a0d52c
abdullah-the-coder <ashrfbdallh052@gmail.com>
2025-09-04 12:19:11 -0700

    first commit for lab3

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Shop from "./pages/Shop";
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <Link to="/">Home</Link> |{" "}
+          <Link to="/about">About</Link> |{" "}
+          <Link to="/shop">Shop</Link>
+        </nav>
+        <hr />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/shop/*" element={<Shop />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
---
+++ src/main.jsx
@@ -0,0 +1,30 @@
+// src/main.jsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Shop from "./pages/Shop";
+
+function App() {
+  return (
+    <BrowserRouter>
+      {/* Navbar */}
+      <nav style={{ display: "flex", gap: "20px", padding: "10px", background: "#eee" }}>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/shop">Shop</Link>
+      </nav>
+
+      {/* Routes */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/shop/*" element={<Shop />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
